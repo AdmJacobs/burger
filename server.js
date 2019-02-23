639dbdb757e003abb382ca16754429f427ada588
Adam <adam1acobs@gmail.com>
2019-02-22 21:21:07 -0700

    heroku fix 2

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
+// Sets up the Express App
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 //middleware
 app.use(express.static(process.cwd() + "/public"));
@@ -19,7 +21,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //routing
-app.use("/", routes);
+app.use('/', routes);
 
 //port listener
 app.listen(process.env.PORT || 3000, function () {
